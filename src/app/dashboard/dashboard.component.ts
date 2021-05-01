cb2e7f3b0542b7f46e4baab76b750a646858f0e6
Leonardo Setubal <setubal43@gmail.com>
2021-05-01 18:38:00 -0300

    .

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.styl']
 })
 export class DashboardComponent implements OnInit {
+  public listUsers = []
+  countF: number;
+  countM: number;
+  countO: number;
+  countN:number;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public userCadastroEvent(event){
+    console.log(event)
+    this.listUsers = event
+  }
+
+  count(){
+    return
+    this.listUsers.map(user => {
+      if(user.sexo === "F"){
+        this.countF +=1;
+      }
+      else if (user.sexo === "M"){
+        this.countM +=1;
+      }
+      else if (user.sexo === "O"){
+        this.countO +=1;
+      }
+      else {
+        this.countN +=1;
+      }
+    })
+  }
 }
