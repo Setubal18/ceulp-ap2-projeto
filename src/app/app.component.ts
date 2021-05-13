ddf4c8d5b95b4c5e6da336ceeedd2c7cfd58441f
Leonardo Setubal <setubal43@gmail.com>
2021-05-13 19:43:16 -0300

    feat: adicionado logRocket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import LogRocket from 'logrocket';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.styl']
+  styleUrls: ['./app.component.styl'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ceulp-api2-projeto';
+
+  ngOnInit(): void {
+
+  }
+
+  initLog(){
+    LogRocket.identify(environment.LOGROCKET)
+  }
 }
